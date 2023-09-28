ce5be175e32de0db69212f1a7ffe2e06566b2434
CKATEPTb <taksis@ex.ua>
2023-09-28 03:37:51 +0300

    Add cooldown conditional to PlayerAbilityUser#canUse

--- src/main/java/dev/ckateptb/minecraft/abilityslots/user/PlayerAbilityUser.java
+++ src/main/java/dev/ckateptb/minecraft/abilityslots/user/PlayerAbilityUser.java
@@ -105,6 +105,7 @@ public class PlayerAbilityUser extends PlayerAbilityTarget implements AbilityUse
                 .hasPermission()
                 .hasCategory()
                 .isEnabled()
+                .withoutCooldown()
                 .build().matches(this, ability);
     }
 
