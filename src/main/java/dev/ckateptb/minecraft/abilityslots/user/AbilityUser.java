a9f17f111a81a4abc97632273aa4cb8f2ac0c492
CKATEPTb <taksis@ex.ua>
2023-09-28 04:16:31 +0300

    Only living entity can use ability

--- src/main/java/dev/ckateptb/minecraft/abilityslots/user/AbilityUser.java
+++ src/main/java/dev/ckateptb/minecraft/abilityslots/user/AbilityUser.java
@@ -5,9 +5,10 @@ import dev.ckateptb.minecraft.abilityslots.ability.holder.AbilityHolder;
 import dev.ckateptb.minecraft.abilityslots.cooldown.CooldownHolder;
 import dev.ckateptb.minecraft.abilityslots.energy.EnergyHolder;
 import dev.ckateptb.minecraft.abilityslots.entity.AbilityTarget;
+import org.bukkit.entity.LivingEntity;
 
 import java.util.stream.Stream;
 
-public interface AbilityUser extends AbilityTarget, CooldownHolder, EnergyHolder, AbilityHolder {
+public interface AbilityUser extends AbilityTarget, LivingEntity, CooldownHolder, EnergyHolder, AbilityHolder {
     Stream<Ability> getAbilityInstances();
 }
