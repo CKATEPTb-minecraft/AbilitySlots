3f625f3dde4634861e2a79a6a360b68f4a899737
CKATEPTb <taksis@ex.ua>
2023-10-04 06:13:03 +0300

    Make AbilityCreateEvent async

--- src/main/java/dev/ckateptb/minecraft/abilityslots/event/AbilityCreateEvent.java
+++ src/main/java/dev/ckateptb/minecraft/abilityslots/event/AbilityCreateEvent.java
@@ -2,16 +2,19 @@ package dev.ckateptb.minecraft.abilityslots.event;
 
 import dev.ckateptb.minecraft.abilityslots.ability.Ability;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
-@RequiredArgsConstructor
 public class AbilityCreateEvent extends Event {
     private final Ability ability;
     private static final HandlerList HANDLERS = new HandlerList();
+
+    public AbilityCreateEvent(Ability ability) {
+        super(true);
+        this.ability = ability;
+    }
 
     public static HandlerList getHandlerList() {
         return HANDLERS;
